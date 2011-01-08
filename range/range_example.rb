7af93215929d87dbfb96ff48e5351546b6fe09e4
Kedar Mhaswade <kedar.mhaswade@gmail.com>
2011-01-08 14:55:41 -0800

    added range example

--- range/range_example.rb
+++ range/range_example.rb
@@ -1,7 +1,52 @@
 #!/usr/bin/env ruby
 =begin
-   Implements an entity whose only discrete Ranges make sense.
-   @see Range
+   Implements an entity whose discrete Ranges make sense. Thus, it defines
+   a method {#succ} because it is possible to find out the successor of an instance
+   of this class.
+   @see {Range}
 =end
-class 
+class Discrete
+  attr_reader :n, :log
 
+=begin
+  Creates an instance of this element, based on the given number.
+  @param [Integer] n associated number
+=end
+  def initialize(n)
+    @n = n
+  end
+
+=begin
+  Returns the successor of this element.
+=end
+
+=begin
+  Overrides Object#<=>, the well known spaceship operator.
+=end
+  def <=>(other)
+    debug("<=> called for #{n}")
+    n.<=> other.n
+  end
+
+  def eql?(other)
+    debug("eql? called for #{n}")
+    n.eql? other.n
+  end
+
+  def ==(other)
+    n == other.n
+  end
+
+  private
+
+  def debug(s)
+    puts "debug: #{s}" if $DEBUG
+  end
+end
+
+f = Discrete.new(10)
+l = Discrete.new(20)
+n = Discrete.new(11)
+r = Range.new(f, l)
+puts r.cover?(n)
+puts r.include?(n)
